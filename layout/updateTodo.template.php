2f2e11f3c903792a1e23de1762fb2fbc08c28c08
Lab. Informatica <119664603+Krotanus@users.noreply.github.com>
2023-12-31 17:08:33 +0100

    Update updateTodo.template.php
    add $_SESSION[] validator

--- layout/updateTodo.template.php
+++ layout/updateTodo.template.php
@@ -1,15 +1,22 @@
 <?php session_start() ?>
-<?php require_once __DIR__ . "/../components/header.component.php"; ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../style/style.css">
+    <title>Project01</title>
+</head>
 <body>
-    <?php require_once __DIR__ . "/../components/menu.component.php"; ?>
-    
-    <form action="./../controller/todo.controller.php" method="POST">
+ <?php require_once __DIR__ ."/../components/menu.component.php";
+     if(isset($_SESSION["user"])) { ?>
 
-        <input type="text" name="descrizione">
-        <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
+  <form action="./../controller/todo.controller.php" method="POST">
+
+        <input type="text" name="descrizione" value="<?=$_GET['descrizione']?>">
+        <input type="hidden" name="id" value="<?=$_GET['id']?>">
         <button type="submit" name="action" value="update">Modifica</button>
     </form>
+<?php }else{header("Location:https://www.sliptstream.it/project/01/");}?>
 </body>
-
 </html>
